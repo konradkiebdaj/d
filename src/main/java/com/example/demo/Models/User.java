a592ddcd430ebead1983e143f6932db1e14462c3
DESKTOP-G8NFVS1\Konrad <konradkiebdaj7@gmail.com>
2019-03-16 13:16:03 +0100

    Add field 'lond id' in User class. Remove two classes 'LoginService' and 'LoginUserImpl'

--- src/main/java/com/example/demo/Models/User.java
+++ src/main/java/com/example/demo/Models/User.java
@@ -2,11 +2,29 @@ package com.example.demo.Models;
 
 public class User {
 
+    private long id;
     private String name;
     private String surname;
     private String email;
     private String password;
     private String confirmPassword;
+
+    public User(String name, String surname, String email, String password, String confirmPassword) {
+        super();
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+        this.password = password;
+        this.confirmPassword = confirmPassword;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
 
     public String getName() {
         return name;
--- src/main/java/com/example/demo/Services/LoginServiceImpl.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.Services;
-
-
-public interface LoginServiceImpl {
-
-    public String login();
-}
